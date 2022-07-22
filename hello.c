efcb1285b71aa39075780178fec2f70f990bed5b
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-07-22 00:06:40 -0300

    0.1

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("hello, world\n");
+}
+
+
+
+
+
+
+
+
+/* Usamos clang no terminal para execusao, porem e somente nesse modelo em outros codigos de c pode usar outra coisa */
+/* clang -o hello.c */
+/* utilizamos rm a.out para remover algo */
